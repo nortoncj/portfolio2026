3c0d5ecc94ebf79e261fe8a21b9604449f09914e
Chris Norton <contact@chrisnortonjr.com>
2026-03-12 16:35:20 -0400

    sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,9 +3,13 @@ import { MetadataRoute } from "next";
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const baseUrl = "https://chrisnortonjr.com";
 
-  const posts = await fetch("https://chrisnortonjr.com/api/insights").then(
-    (res) => res.json(),
-  );
+  const res = await fetch(`${baseUrl}/api/insights`, {
+    cache: "no-store",
+  });
+
+  if (!res.ok) return [];
+
+  const posts = await res.json();
 
   const insightRoutes = posts.map((post: any) => ({
     url: `${baseUrl}/insights/${post.slug}`,
