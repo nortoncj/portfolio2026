168d84068f85ee394303012b4c531e6a14a8be04
Chris Norton <contact@chrisnortonjr.com>
2026-04-01 20:25:25 -0400

    layout meta

--- app/(home)/layout.tsx
+++ app/(home)/layout.tsx
@@ -5,7 +5,7 @@ import Footer from "@/components/sections/footer/Footer2";
 import { buildMetadata } from "../layout";
 
 export const metadata: Metadata = buildMetadata({
-  title: "Chris Norton Jr | Engineer",
+  title: "Engineer",
   description:
     "Full-Stack Developer. Cloud Engineer. Marketing Technologist. Projects, breakdowns, and insights across DevOps, Embedded Systems, and Web Development — built to inform, impress, and collaborate.",
 });
