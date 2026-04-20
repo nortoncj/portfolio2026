c5f852f868bda5e74f77b4453226a8603edd2b62
Chris Norton <contact@chrisnortonjr.com>
2026-04-20 11:07:20 -0400

    clarity fix2

--- app/(home)/clarity.tsx
+++ app/(home)/clarity.tsx
@@ -3,11 +3,10 @@
 import { useEffect } from "react";
 import { clarity } from "clarity-js";
 
+
 export function Clarity() {
   useEffect(() => {
-    clarity.start({
-      projectId: process.env.NEXT_PUBLIC_CLARITY_ID || "yourProjectId",
-    });
+    clarity("start", process.env.NEXT_PUBLIC_CLARITY_ID || "yourProjectId");
   }, []);
 
   return null;
