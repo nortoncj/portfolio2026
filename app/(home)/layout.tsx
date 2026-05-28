0ef816d6c131ac15fa010692d63bbaf21e159f62
Chris Norton <contact@chrisnortonjr.com>
2026-05-28 15:37:06 -0400

    ga-4

--- app/(home)/layout.tsx
+++ app/(home)/layout.tsx
@@ -15,18 +15,32 @@ export default function FrontLayout({
   return (
     <>
       <MainHeader />
+      {/* Google Tag Manager */}
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-S5ZBPZ262W"
+        strategy="afterInteractive"
+      />
+
+      <Script id="gtag-init" strategy="afterInteractive">
+        {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-S5ZBPZ262W');
+          `}
+      </Script>
       {children}
       {clarityId && (
-  <Script id="ms-clarity" strategy="afterInteractive">
-    {`
+        <Script id="ms-clarity" strategy="afterInteractive">
+          {`
       (function(c,l,a,r,i,t,y){
         c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
         t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
         y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
       })(window, document, "clarity", "script", "${clarityId}");
     `}
-  </Script>
-)}
+        </Script>
+      )}
       <Footer />
     </>
   );
