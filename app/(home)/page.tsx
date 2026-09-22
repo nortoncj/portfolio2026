c91af571f7b27c60fda2c49f8f902e470f407acd
Chris Norton <contact@chrisnortonjr.com>
2026-09-22 02:44:59 -0400

    metadata update

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -12,7 +12,7 @@ import { Metadata } from "next";
 export const metadata: Metadata = buildMetadata({
   title: "DevOps, Cloud & Embedded Engineer",
   description:
-    " I build AWS infrastructure, CI/CD pipelines, firmware automation, and IoT systems",
+    " I build Devops infrastructures, firmware automation, and IoT systems",
   image: "/og-image.png",
   url: "https://chrisnortonjr.com",
 });
