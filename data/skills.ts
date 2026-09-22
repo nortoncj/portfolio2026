c7cd01c362c6333fc8c4c1feba23465a8d5eb5f8
Chris Norton <contact@chrisnortonjr.com>
2026-09-22 02:41:58 -0400

    spelling fix

--- data/skills.ts
+++ data/skills.ts
@@ -746,7 +746,7 @@ export const education: Degree = {
 export const certifications: Cert[] = [
   {
     id: 1,
-    title: "AWS Certified Cloud Practioner",
+    title: "AWS Certified Cloud  practitioner",
     issuer: "Amazon Web Services",
     description:
       "Validated expertise in designing distributed systems on AWS infrastructure.",
